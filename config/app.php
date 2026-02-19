64c71eaf23c455518f0d55118afae30b3c6ccc0c
이정호 <ljh3919@gmail.com>
2026-02-19 18:27:17 +0900

    500 서버 에러 수정

--- config/app.php
+++ config/app.php
@@ -97,7 +97,7 @@ return [
 
     'cipher' => 'AES-256-CBC',
 
-    'key' => env('APP_KEY'),
+    'key' => env('APP_KEY', 'base64:i0sEWcW/MYbkbmFRLCOU1s/e7DE30cGt6h4uQmCvj2k='),
 
     'previous_keys' => [
         ...array_filter(
