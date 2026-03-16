1270d82090a657143ec70cf94a9069716f9ca8db
이정호 <ljh3919@gmail.com>
2026-03-16 17:42:32 +0900

    소스 원복

--- app/Mail/Transport/PhpMailTransport.php
+++ app/Mail/Transport/PhpMailTransport.php
@@ -28,37 +28,26 @@ class PhpMailTransport extends AbstractTransport
         // 제목 (UTF-8 인코딩 명시)
         $subject = '=?UTF-8?B?' . base64_encode($email->getSubject()) . '?=';
         
-        // 헤더 복제 및 To, Subject 제거 (mail 함수 오동작 방지)
-        $headers = clone $email->getHeaders();
-        $headers->remove('To');
-        $headers->remove('Subject');
+        // 메일 본문
+        $body = $email->getHtmlBody() ?: $email->getTextBody();
+        $contentType = $email->getHtmlBody() ? 'text/html' : 'text/plain';
         
-        // 발신자 주소 추출 (Dothome의 -f 옵션용)
-        $fromAddress = config('mail.from.address');
-        if ($from = $email->getFrom()) {
-            $fromAddress = $from[0]->getAddress();
-        }
+        // 발신자 주소 (Dothome 환경에서는 계정명과 일치하는 메일 주소를 사용하는 것이 필수적일 수 있음)
+        $from = $email->getFrom();
+        $senderAddress = !empty($from) ? $from[0]->getAddress() : config('mail.from.address');
         
-        // 헤더 문자열 생성 (불필요한 공백/줄바꿈 제거)
-        $headerString = trim($headers->toString());
-        
-        // 본문 데이터 추출
-        $body = $email->getBody()->toString();
-        
-        // 닷홈(Linux) 환경의 mail() 함수는 \n을 선호하므로 줄바꿈 통일
-        $headerString = str_replace("\r\n", "\n", $headerString);
-        $body = str_replace("\r\n", "\n", $body);
-        
-        // Dothome 등 일부 호스팅에서는 발신자 주소 고정(-f) 옵션이 필수적인 경우가 많음
-        $extraParams = "-f" . $fromAddress;
+        // 가장 안정적인 최소한의 헤더 구성
+        $headers = [];
+        // $headers[] = "MIME-Version: 1.0";
+        $headers[] = "Content-type: $contentType; charset=utf-8";
+        $headers[] = "From: $senderAddress";
+        $headers[] = "Reply-To: $senderAddress";
 
-        \Log::info("Attempting PHP mail() to $to with -f $fromAddress");
+        $headerString = implode("\n", $headers);
 
         // 발송 시도
-        if (!mail($to, $subject, $body, $headerString, $extraParams)) {
-            \Log::error("PHP mail() failed to $to with params: $extraParams");
-            // 디버깅을 위해 에러 로그 상세화
-            \Log::error("Failed headers: " . $headerString);
+        if (!mail($to, $subject, $body, $headerString)) {
+            \Log::error("PHP mail() failed to $to");
             throw new \RuntimeException('PHP mail() 함수를 통한 메일 발송에 실패했습니다.');
         }
     }
